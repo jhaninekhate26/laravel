915fe64ae0e299897d6f52d759893b78914ad774
Janine Kate Nobleza <Janine Kate Nobleza>
2017-01-09 15:47:28 +0800

    New Commit

--- laravel/database/migrations/2017_01_05_152135_create_users_table.php
+++ laravel/database/migrations/2017_01_05_152135_create_users_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
@@ -14,13 +13,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->timestamps();
+            $table->string('email');
+            $table->string('first_name');
             $table->string('password');
             $table->rememberToken();
-            $table->timestamps();
         });
-    }
+    }  
 
     /**
      * Reverse the migrations.
