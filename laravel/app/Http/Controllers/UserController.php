523a5f6c5bfa59f923d388886e84ff36a959de28
Janine Kate Nobleza <Janine Kate Nobleza>
2017-01-17 19:32:50 +0800

    new

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -2,19 +2,18 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use Illuminate\Http\Request;
+use Illuminate\Http\Request; 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
-	public function getDashboard()
-	{
-		return view('dashboard');
-	}
+	
 
 	public function postSignUp(Request $request)
 	{
-
 		$this->validate($request, [
 			'email' => 'required|email|unique:users',
 			'first_name' => 'required|max:120',
@@ -29,12 +28,12 @@ class UserController extends Controller
 		$user->email = $email;
 		$user->first_name = $first_name;
 		$user->password = $password;
-
 		$user->save();
 
 		Auth::login($user);
 
 		return redirect()->route('dashboard');
+
 	}
 
 	public function postSignIn(Request $request)
@@ -44,9 +43,40 @@ class UserController extends Controller
 			'password' => 'required'
 		]);
 
-		if (Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
+		if(Auth::attempt(['email' => $request['email'], 'password' => $request['password']])) {
 			return redirect()->route('dashboard');
 		}
 		return redirect()->back();
+
 	}
-}
+
+	public function getLogout() {
+		Auth::logout();
+		return redirect()->route('home');
+	}
+
+	public function getAccount() {
+		return view('account', ['user' => Auth::user()]);
+	}
+
+	public function postSaveAccount(Request $request) {
+		$this->validate($request, [
+			'first_name' => 'required|max:120'
+			]);
+
+		$user = Auth::user();
+		$user->first_name = $request['first_name'];
+		$user->update();
+		$file = $request->file('image');
+		$filename = $request['first_name'] . '-' . $user->id . '.jpg';
+		if ($file) {
+			Storage::disk('local')->put($filename, File::get($file));
+		}
+		return redirect()->route('account');
+	}
+
+	public function getUserImage($filename) {
+		$file = Storage::disk('local') ->get($filename);
+		return new Response($file, 200);
+	}
+} 
--- laravel/database/migrations/2017_01_15_143922_create_posts_table.php
+++ laravel/database/migrations/2017_01_15_143922_create_posts_table.php
@@ -1,8 +1,9 @@
 <?php
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -11,15 +12,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->string('email');
-            $table->string('first_name');
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('body');
+            $table->integer('user_id');
         });
-    }  
+    }
 
     /**
      * Reverse the migrations.
@@ -28,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::drop('posts');
     }
 }
